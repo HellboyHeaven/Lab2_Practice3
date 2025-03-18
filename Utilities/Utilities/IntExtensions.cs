d48358da44fa4b05e699fc8b20549d07b31cb41d
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2025-03-18 20:22:52 +0100

    done

--- Utilities/Utilities/IntExtensions.cs
+++ Utilities/Utilities/IntExtensions.cs
@@ -2,5 +2,41 @@
 
 public static class IntExtensions
 {
-    //your code goes here
+    public static string ToOrdinal(this int num)
+    {
+        switch (num % 100)
+        {
+            case 11:
+                return $"{num}th";
+            case 12:
+                return $"{num}th";
+            case 13:
+                return $"{num}th";
+            default:
+                break;
+        }
+
+
+        switch (num % 10)
+        {
+            case 1:
+                return $"{num}st";
+            case 2:
+                return $"{num}nd";
+            case 3:
+                return $"{num}rd";
+            default:
+                return $"{num}th";
+        } 
+    }
+
+    public static bool IsPrime(this int num)
+    {
+        for (int i = 1; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+                return true;
+        }
+        return false;
+    }
 }
--- Utilities/Utilities/StringExtensions.cs
+++ Utilities/Utilities/StringExtensions.cs
@@ -1,6 +1,14 @@
-﻿namespace Utilities;
+﻿using System.Text.RegularExpressions;
+
+namespace Utilities;
 
 public static class StringExtensions
 {
-    //your code goes here
+    public static string ToSnakeCase(this string text)
+    {
+        return Regex.Replace(text.Trim(), @"\W+", " ")  // Replace non-word characters with space
+                    .ToLower()                           // Convert to lowercase
+                    .Trim()                              // Trim again in case of leading/trailing spaces
+                    .Replace(" ", "_"); ;
+    }
 }
